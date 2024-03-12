995e7d4444c695a75ba61de192e233fb03ad2e54
Nguyen Vu <nguyentuanvuk62@gmail.com>
2024-03-12 12:22:23 +0700

    updated at 12/03/2024

--- register.js
+++ register.js
@@ -13,7 +13,7 @@ router.get('/register', (req, res) => {
 });
 
 router.post('/register', (req, res) => {
-  const {name, userName, password, studentID, mail, phone, facebook, avatar} = req.body;
+  const {name, userName, password, mail, phone, facebook, avatar} = req.body;
 
   // Check if username already exists
   const checkUserSql = `SELECT * FROM user WHERE userName = ?`;
@@ -30,25 +30,10 @@ router.post('/register', (req, res) => {
           return;
       }
 
-      // Check if student ID already exists
-      const checkStudentIdSql = `SELECT * FROM user WHERE studentID = ?`;
-      db.get(checkStudentIdSql, [studentID], (err, row) => {
-          if (err) {
-              console.error('Error querying database for student ID: ', err.message);
-              res.status(500).json({error: 'Internal server error'});
-              return;
-          }
+          // If username do not exist, insert the new user
+          const insertSql =  `INSERT INTO user (name, userName, password, mail, phone, facebook, avatar) VALUES (?, ?, ?, ?, ?, ?, ?)`;
 
-          if (row) {
-              // If a row is found, it means the student ID already exists
-              res.status(400).json({error: 'Student ID already exists'});
-              return;
-          }
-
-          // If username and student ID do not exist, insert the new user
-          const insertSql =  `INSERT INTO user (name, userName, password, studentID, mail, phone, facebook, avatar) VALUES (?, ?, ?, ?, ?, ?, ?, ?)`;
-
-          db.run(insertSql, [name, userName, password, studentID, mail, phone, facebook, avatar], function(err) {
+          db.run(insertSql, [name, userName, password, mail, phone, facebook, avatar], function(err) {
               if (err) {
                   console.error('Error inserting data: ', err.message);
                   res.status(500).json({error: 'Failed to insert new user'});
@@ -56,9 +41,8 @@ router.post('/register', (req, res) => {
               }
               
               console.log(`A new row has been inserted with rowid ${this.lastID}`);
-              res.json({name: name, userName: userName, studentID: studentID, mail: mail, phone: phone, facebook: facebook, avatar: avatar});
+              res.json({name: name, userName: userName, mail: mail, phone: phone, facebook: facebook, avatar: avatar});
           });
-      });
   });
 });
 
