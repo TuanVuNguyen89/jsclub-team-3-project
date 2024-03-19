b6e77ed541eef29a8d0da70e767b50bd84dba9ca
ducannh <ducanhdeptraile651@gmail.com>
2024-03-19 23:25:02 +0700

    ducanhsua12

--- homejs/home2.js
+++ homejs/home2.js
@@ -13,26 +13,26 @@ var swiper = new Swiper(".mySwiper", {
     },
 });
 
-// document.addEventListener('DOMContentLoaded', function () {
-//     var swiper = new Swiper('.mySwiper', {
-//         slidesPerView: 1,
-//     grabCursor: true,
-//     loop: true,
-//     pagination: {
-//       el: ".swiper-pagination",
-//       clickable: true,
-//     },
-//     });
+document.addEventListener('DOMContentLoaded', function () {
+  var swiper = new Swiper('.mySwiper', {
+      slidesPerView: 1,
+      grabCursor: true,
+      loop: true,
+      pagination: {
+          el: ".swiper-pagination",
+          clickable: true,
+      },
+  });
 
-//     var navButtons = document.querySelectorAll('.nav-btn');
+  var navButtons = document.querySelectorAll('.nav-btn');
 
-//     navButtons.forEach(function (button) {
-//         button.addEventListener('click', function () {
-//             if (this.classList.contains('.swiper-button-next')) {
-//                 swiper.slideNext();
-//             } else if (this.classList.contains('.swiper-button-prev')) {
-//                 swiper.slidePrev();
-//             }
-//         });
-//     });
-// });
+  navButtons.forEach(function (button) {
+      button.addEventListener('click', function () {
+          if (this.classList.contains('swiper-button-next')) {
+              swiper.slideNext(); // Lướt sang trang kế tiếp
+          } else if (this.classList.contains('swiper-button-prev')) {
+              swiper.slidePrev(); // Lướt sang trang trước đó
+          }
+      });
+  });
+});
