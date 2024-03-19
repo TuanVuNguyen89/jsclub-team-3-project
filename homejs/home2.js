a98f28708f7f91c575ede30110b7daaf827a73f1
ndkhoi192 <ndkhoi192@gmail.com>
2024-03-19 19:39:51 +0700

    updated

--- homejs/home2.js
+++ homejs/home2.js
@@ -12,3 +12,27 @@ var swiper = new Swiper(".mySwiper", {
       prevEl: ".swiper-button-prev",
     },
 });
+
+// document.addEventListener('DOMContentLoaded', function () {
+//     var swiper = new Swiper('.mySwiper', {
+//         slidesPerView: 1,
+//     grabCursor: true,
+//     loop: true,
+//     pagination: {
+//       el: ".swiper-pagination",
+//       clickable: true,
+//     },
+//     });
+
+//     var navButtons = document.querySelectorAll('.nav-btn');
+
+//     navButtons.forEach(function (button) {
+//         button.addEventListener('click', function () {
+//             if (this.classList.contains('.swiper-button-next')) {
+//                 swiper.slideNext();
+//             } else if (this.classList.contains('.swiper-button-prev')) {
+//                 swiper.slidePrev();
+//             }
+//         });
+//     });
+// });
